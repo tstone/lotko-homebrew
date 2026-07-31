370bb7979a885ca9656099fdaecb258b5513c488
Titus <wastingtape@gmail.com>
2026-07-30 18:09:12 -0600

    Add missing tags

--- src/hardware/drop_bank.rs
+++ src/hardware/drop_bank.rs
@@ -26,9 +26,15 @@ hardware_defs! {
   pub TARGET3: SwitchDefinition = SwitchDefinition::new("drop_target3");
   pub PADDLE_SWITCH: SwitchDefinition = SwitchDefinition::new("drop_paddle");
 
-  pub TARGET1_LEDS: LedDefinition = LedDefinition::strip("target1", 4);
-  pub TARGET2_LEDS: LedDefinition = LedDefinition::strip("target2", 4);
-  pub TARGET3_LEDS: LedDefinition = LedDefinition::strip("target3", 4);
+  pub TARGET1_LEDS: LedDefinition = LedDefinition::strip("target1", 4)
+    .tag(Playfield);
 
-  pub PADDLE_LED: LedDefinition = LedDefinition::single("paddle");
+  pub TARGET2_LEDS: LedDefinition = LedDefinition::strip("target2", 4)
+    .tag(Playfield);
+
+  pub TARGET3_LEDS: LedDefinition = LedDefinition::strip("target3", 4)
+    .tag(Playfield);
+
+  pub PADDLE_LED: LedDefinition = LedDefinition::single("paddle")
+    .tag(Playfield);
 }
--- src/hardware/upper_pass_lane.rs
+++ src/hardware/upper_pass_lane.rs
@@ -6,13 +6,16 @@ use crate::hardware::more_tags::*;
 hardware_defs! {
   pub SPINNER: LedDefinition = LedDefinition::single("upper_pass_spinner")
     .tag(Circle)
-    .tag(Lane);
+    .tag(Lane)
+    .tag(Playfield);
 
   pub ARROW_LED1: LedDefinition = LedDefinition::single("upper_pass_arrow1")
     .tag(SmallArrow)
-    .tag(Lane);
+    .tag(Lane)
+    .tag(Playfield);
 
   pub ARROW_LED2: LedDefinition = LedDefinition::single("upper_pass_arrow2")
     .tag(SmallArrow)
-    .tag(Lane);
+    .tag(Lane)
+    .tag(Playfield);
 }
